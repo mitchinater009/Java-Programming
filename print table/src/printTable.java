7365f242a342c069bba53aaf291af83ffe17f33a
Eric Roberts <eric.dan.roberts@gmail.com>
2016-06-21 21:38:52 -0700

    print table

---
+++ print table/src/printTable.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author EricRoberts
+ *
+ */
+public class printTable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//this will print the table.
+		System.out.println("a    a^2   a^3");
+		System.out.println("1    1	   1");
+		System.out.println("2    4	   8");
+		System.out.println("3    9	   27");
+		System.out.println("4    16	   64");
+	}
+
+}
