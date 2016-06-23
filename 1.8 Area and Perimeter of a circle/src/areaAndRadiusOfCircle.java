f91f0208ae6aee60081100108eee49ebfca275e0
Eric Roberts <eric.dan.roberts@gmail.com>
2016-06-22 21:19:43 -0700

    1.8 Area and Perimeter of Circle

---
+++ 1.8 Area and Perimeter of a circle/src/areaAndRadiusOfCircle.java
@@ -0,0 +1,13 @@
+
+public class areaAndRadiusOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14159; //Declare a constant
+		double perimeter = 2 * 5.5 * PI;
+		double area = 5.5 * 5.5 * PI;
+		
+			System.out.println("The area of the circle is "+area+ " and the perimeter is "+perimeter);
+	}
+
+}
