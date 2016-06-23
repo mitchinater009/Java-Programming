9868a0762c154f213d53c250774f15a61b17f813
Eric Roberts <eric.dan.roberts@gmail.com>
2016-06-22 21:36:21 -0700

    1.10 Average Speed

---
+++ 1.10 Average Speed/src/averageSpeed.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author EricRoberts
+ *
+ */
+public class averageSpeed {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double mile = 14 / 1.6; //converting kilometers to miles
+		
+		System.out.println("The runner's average speed is "+mile+" miles per hour.");
+
+	}
+
+}
