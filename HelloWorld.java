cfca8858ec518ae682798cb1e4269a70e46952f0
Hebertla <Lisa.Hebert@my.gcu.edu>
2017-05-29 21:29:53 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+/** Program: Hello World
+* File: HelloWorld.java 
+* Summary: Prints two lines of string literal output to the console; 
+           my name and greeting. 
+* Author: Lisa A. Hebert
+* Date: May 30, 2017
+**/
+
+
+public class HelloWorld
+{  // begin HelloWorld class 
+
+	// The start of the main method is below.  Java execution begins in the main method 
+	public static void main(String[] args)
+	{  // begin main method 
+
+		//Prints out two lines of string to the console. 
+		System.out.println("Hi, my name is Lisa Hebert.");
+		System.out.println("Welcome, Class!");
+
+	} // End of the main method 
+
+} //End of the HelloWorld class 
